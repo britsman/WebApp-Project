355bb01baf4d6f4152b9775244161868b2aed39a
Hannes <Hannes@129.16.190.219>
2012-10-19 15:57:33 +0200

    Lite små fix här och där

--- Library/src/main/java/com/mycompany/library/beans/RegisterPageBB.java
+++ Library/src/main/java/com/mycompany/library/beans/RegisterPageBB.java
@@ -43,6 +43,7 @@ public class RegisterPageBB implements Serializable {
                     triedRegister = true;
                 }
             }
+            
         }
         else{
             completeRegistration();
